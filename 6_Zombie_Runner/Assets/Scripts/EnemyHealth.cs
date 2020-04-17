891d2619c98fe12fa06de1df580e6a1427fb927d
Hayden Avery Matthews <haydenamatthews@yahoo.com>
2020-04-16 19:06:23 -0600

    +Added drawing gizmo to enemy object +Added provocation logic to enemy AI +Gave the player a gun and made it shoot +Implemented raycasting +Implemented enemy health +Added particles to muzzle and on hit with an object +Added the animator controller for the enemy

---
+++ 6_Zombie_Runner/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] float hitPoints = 100f;
+
+    //Create a public method which reduces hitpoints
+    public void TakeDamage(float damage)
+    {
+        hitPoints -= damage;
+        if (hitPoints <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
